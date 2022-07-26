b7a4905a3bc577f04582016757d1b4c09ce9fbb8
CharmingDays <nawhpongmaji1@gmail.com>
2022-07-26 08:24:33 -0500

    added ban/pick/hover phases

--- Python/lcu_client.py
+++ Python/lcu_client.py
@@ -1,24 +1,57 @@
+import typing
 import requests as rq
 from requests import Response
 import time
 import base64
-import lcu_connector_python as lcu
 import os,json
 from datetime import datetime
-from lcu_driver import Connector
-import keyboard
+import subprocess
+import sys
+import pkg_resources
+def check_and_install_requests():
+    #INSTALLS MISSING LIBS IF NOT FOUND
+    required = {'requests'}
+    installed = {pkg.key for pkg in pkg_resources.working_set}
+    missing = required - installed
+    if missing:
+        python = sys.executable
+        subprocess.check_call([python,'-m','pip','install',*missing],stdout=subprocess.DEVNULL)
+
+
+check_and_install_requests()
+
 
 class LCU(object):
     def __init__(self):
-        self._local=lcu.connect()
-        self.token=base64.b64encode(f'riot:{self._local["authorization"]}'.encode()).decode()
-        self.url="https://"+self._local['url']
+        self.lockfile_setup()
         self.headers={'Authorization':f"Basic {self.token}",'Accept': 'application/json'}
         self.session=rq.Session()
         # self.session.verify=os.path.join(os.path.dirname(os.path.abspath(__file__)),'riotgames.pem')
         self.riotgames_SSL()
         self.session.headers=self.headers
-        self.fullBanPosition=1
+
+
+    def lockfile_setup(self,filePath:str=None):
+        """
+        Look for the lockfile and setup token and url from lockfile.
+        NOTE: THE CLASS WON'T BE ABLE TO MAKE REQUESTS WITHOUT THE LOCKFILE SETUP
+        """
+        lockfile = r'C:\Riot Games\League of Legends\lockfile'
+        if filePath:
+            lockfile = filePath
+
+        if not os.path.exists(lockfile):
+            raise FileExistsError(lockfile)
+        else:
+            lockfile = open(lockfile,'r')
+        rawData = lockfile.read()
+        lockfile.close()
+        rawData = rawData.split(":")
+        data = {"port":rawData[2],"auth":rawData[3],"method":rawData[4]+"://127.0.0.1"}
+        token = base64.b64encode(f'riot:{data["auth"]}'.encode()).decode()
+        url = f"{data['method']}:{data['port']}"
+        setattr(self,'token',token)
+        setattr(self,'url',url)
 
 
     def riotgames_SSL(self):
@@ -39,14 +72,20 @@ class LCU(object):
 
 
     def leave_lobby(self):
+        """
+        Leaves the lobby
+        NOTE: DO NOT RUN THIS FUNCTION WHILE STILL DURING THE HONOR TEAMMATE SCREEN TO AVOID BLACK SCREEN
+        """
         url=self.url+"/lol-lobby/v2/lobby"
         data = self.session.delete(url)
         return data
     
-    def create_lobby(self,queue_type:str="DRAFT"):
+    def create_lobby(self,queue_type:str="DRAFT",primaryRole = None, secondaryRole = "FILL"):
         """
         Create a lobby with given lobby type
         Check the `queue_dict` dict for lobby options
+        THIS FUNCTION CAN ALSO SET THE ROLE POSITIONS IF PROVIDED
+            - NOTE: NOT SETTING THE SECONDARY ROLE WILL MAKE YOU FILL AS SECONDARY 
         """
         queue_type= queue_type.upper()
         queue_dict= {
@@ -81,6 +120,8 @@ class LCU(object):
             "queueId": queue_dict[queue_type]
         }
         lobby_data=self.session.post(url,headers=self.headers,data=json.dumps(payload))
+        if primaryRole:
+            self.role_position(primaryRole,secondaryRole)
         return lobby_data
 
 
@@ -96,6 +137,7 @@ class LCU(object):
             - your secondary role for the game queue 
         """
         url=self.url+"/lol-lobby/v2/lobby/members/localMember/position-preferences"
+        primary,secondary = primary.upper(),secondary.upper()
         payload={
             "firstPreference": primary,
             "secondPreference": secondary
@@ -165,8 +207,7 @@ class LCU(object):
         #This function should return bool to indicate if there's a penalty.
         url = self.url + '/lol-lobby/v2/lobby/matchmaking/search-state'
         data = self.session.get(url).json()
-        data = True if data['state']  == 'InProgress' else False #NOTE: NOT TESTED
-        return data
+        return data['lowPriorityData']['penaltyTimeRemaining']
 
 
     def start_match(self):
@@ -200,51 +241,105 @@ class LCU(object):
 
 
     def champion_select_timer(self):
-        url = self.url+f"/lol-champ-select/v1/session/timer"
+        url = self.url+f"/lol-champ-select/v1/session"
         data = self.session.get(url)
-        return data.text
+        return data.json()['timer']
 
     def locked_in_champion(self):
         url = self.url+'/lol-champ-select/v1/current-champion'
         data = self.session.get(url)
         return data
 
-    def banable_champions(self):
+    def bannable_champions(self):
         url = self.url+"/lol-champ-select/v1/bannable-champion-ids"
         data = self.session.get(url)
         return data
 
-    def pickable_champions(self):
+    def pickable_champions(self) -> Response:
         url = self.url+"/lol-champ-select/v1/pickable-champion-ids"
         data = self.session.get(url)
         return data
 
-
-    def ban_champion(self,championId,fullBan:bool=False):
+    def hover_champion(self,championId:int):
         """
-        fullBan:
-            - indicate whether one summoner will ban for entire team
+        Context:
+        {
+            "actorCellId": 1,
+            "championId": 33,
+            "completed": true,
+            "id": 1,
+            "isAllyAction": true,
+            "isInProgress": false,
+            "pickTurn": 1,
+            "type": "pick"
+        }
+        """
+        phaseData = self.check_action_turn()
+        if not phaseData:
+            #Indicates it's not local player's turn or all actions have been completed.
+            return False
+        actionId = phaseData['id']
+        phaseData['championId'] = championId
+        selectUrl = self.url+f'/lol-champ-select/v1/session/actions/{actionId}'
+        data = self.session.patch(selectUrl,data=json.dumps(phaseData)) #Hover the champion to ban/pick
+        return data
         
-        @return:
-            - returns a tuple of the selectUrl and banData    
+    def ban_pick_champion(self,champion) -> Response:
         """
-        selectUrl = self.url+'/lol-champ-select/v1/session/actions/1'
-        payload = {"championId":championId}
+        @return:
+            - returns a tuple of the selectUrl and banData
+        """
+        if type(champion) != int:
+            nameData = rq.get('https://gist.githubusercontent.com/CharmingDays/6e7d673403439b697b10a2d6100e2288/raw/ff15e0765da4a4d71b73b673cffb20e7d5461a64/champid.json').json()
+            nameData = {key.upper(): value for key,value in nameData.items()}
+            if champion.upper() not in nameData:
+                raise f"Champion {champion} not found"
+            champion = int(nameData[champion.upper()])
 
-        selectData = self.session.patch(selectUrl,data=json.dumps(payload))
-        banUrl = self.url+f'/lol-champ-select/v1/session/actions/{self.fullBanPosition}/complete'
-        if fullBan:
-            self.fullBanPosition+=1
-        banData = self.session.post(banUrl)
-        completeUrl = self.url + f'/lol-champ-select/v1/session/actions/{self.fullBanPosition}/complete'
-        self.session.post(completeUrl)
-        return selectData,banData
-
+        self.hover_champion(champion)
+        phaseData = self.check_action_turn()
+        url = self.url + f"/lol-champ-select/v1/session/actions/{phaseData['id']}/complete"
+        self.session.post(url)
 
     def champion_select_session(self):
+        """
+        404: Not in champion select.
+
+        """
         url = self.url + "/lol-champ-select/v1/session"
         data = self.session.get(url)
         return data
+
+
+
+    def check_action_turn(self) -> dict:
+        """
+        Returns the current incomplete phase of the action
+        False if latest action already completed
+        """
+        data = self.champion_select_session().json()
+        if not data['actions'][-1][0]['completed']:
+            return data['actions'][-1][0]
+
+        return False
+
+
+    def my_turn_pick(self) -> bool:
+        """
+        Checks if it's the local summoner's turn or not.
+        """
+        data = self.champion_select_session().json()
+        if data['actions'][-1][0]['actorCellId'] == data['localPlayerCellId'] and not data['actions'][-1][0]['completed']:
+            return True
+
+        return False
+    
+    def current_champ_select_phase(self) -> str:
+        """
+        Returns the current champion select phase
+        """
+        data = self.champion_select_session().json()
+        return data['actions'][-1][0]['type']
 
 
     def export_friends(self,path=None):
@@ -275,8 +370,15 @@ class LCU(object):
         
         return False
 
+    def bannable_campions(self) -> Response:
+        """
+        """
+        url = self.url + "/lol-champ-select/v1/bannable-champions"
+        data = self.session.get(url)
+        return data
 
-    def logout(self):   
+
+    def logout(self) -> Response:
         """
         Logout the current summoner account
         """
@@ -294,7 +396,16 @@ class LCU(object):
             return False
         return True
 
+ 
 
+
+    def check_selected_champion(self) -> Response:
+        """
+        Checks what champion the local summoner currently has LOCKED IN
+        """
+        url = self.url+"/lol-champ-select/v1/current-champion"
+        data = self.session.get(url)
+        return data
 
 
 
@@ -303,7 +414,7 @@ class LCU(object):
         print(data.text)
         return data
 
-    
+
 
 
 class AutoFunctions(LCU):
@@ -331,10 +442,7 @@ class AutoFunctions(LCU):
 
 
 
-
-
-
-    def auto_accept_match(self,wait_time=3) -> Response:
+    def auto_accept_match(self,wait_time:int=3) -> tuple[Response,bool]:
         """
         The function will not work if the lobby isn't in queue already and will stop when queue has been cancelled
         - wait_time
@@ -357,5 +465,12 @@ class AutoFunctions(LCU):
         return matchmaking_state
 
 
-lol = LCU()
+    def auto_ban_champion(self,championName):
+        pass
+
+    def auto_pick_champion(self,championName):
+        pass
+
+
 autos = AutoFunctions()
+lol = LCU()
