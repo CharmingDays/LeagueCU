0035cdf576899ac36da6032716ba3976a59b0e93
CharmingDays <nawhpongmaji1@gmail.com>
2022-07-28 13:42:48 -0500

    separated functions

--- Python/lcu_client.py
+++ Python/lcu_client.py
@@ -24,17 +24,16 @@ import requests as rq
 from requests import Response
 
 
-class LCU(object):
-    def __init__(self):
-        self.lockfile_setup()
-        self.headers={'Authorization':f"Basic {self.token}",'Accept': 'application/json'}
-        self.session=rq.Session()
-        # self.session.verify=os.path.join(os.path.dirname(os.path.abspath(__file__)),'riotgames.pem')
+
+class LcuSessionSetup(object):
+    def __init__(self) -> None:
+        self.session = rq.Session()
+        self.lockfile()
         self.riotgames_SSL()
-        self.session.headers=self.headers
+        self.session.headers = {'Authorization':f"Basic {self.token}",'Accept': 'application/json'}
 
 
-    def lockfile_setup(self,filePath:str=None):
+    def lockfile(self,filePath:str=None):
         """
         Look for the lockfile and setup token and url from lockfile.
         NOTE: THE CLASS WON'T BE ABLE TO MAKE REQUESTS WITHOUT THE LOCKFILE SETUP
@@ -74,175 +73,17 @@ class LCU(object):
             self.session.verify = r"{}\riotgames.pem".format(os.getcwd())
 
 
-    def leave_lobby(self):
-        """
-        Leaves the lobby
-        NOTE: DO NOT RUN THIS FUNCTION WHILE STILL DURING THE HONOR TEAMMATE SCREEN TO AVOID BLACK SCREEN
-        """
-        url=self.url+"/lol-lobby/v2/lobby"
-        data = self.session.delete(url)
-        return data
-    
-    def create_lobby(self,queue_type:str="DRAFT",primaryRole = None, secondaryRole = "FILL"):
-        """
-        Create a lobby with given lobby type
-        Check the `queue_dict` dict for lobby options
-        THIS FUNCTION CAN ALSO SET THE ROLE POSITIONS IF PROVIDED
-            - NOTE: NOT SETTING THE SECONDARY ROLE WILL MAKE YOU FILL AS SECONDARY 
-        """
-        queue_type= queue_type.upper()
-        queue_dict= {
-            "DRAFT": 400,
-            "RANKED":420,
-            "RANKED_FLEX":440,
-            "ARAM":450,
-            "BLIND":430,
-            "CUSTOM":"CUSTOM_GAME",
-            "PRACTICE":"PRACTICETOOL"
-        }
-        url=self.url+"/lol-lobby/v2/lobby"
-
-        if queue_type in ['CUSTOM_GAME','PRACTICE']:
-            data = {
-                "customGameLobby": {
-                    "configuration": {
-                    "gameMode": f"{queue_dict[queue_type]}", "gameMutator": "", "gameServerRegion": "", "mapId": 11, "mutators": {"id": 1}, "spectatorPolicy": "AllAllowed", "teamSize": 5
-                    },
-                    "lobbyName": "Name",
-                    "lobbyPassword": None
-                },
-                "isCustom": True
-            }
-            self.session.post(url,data=json.dumps(data))
-            
-
-        if queue_type not in queue_dict:
-            return f"Queue type {queue_type} not found."
-
-        payload={
-            "queueId": queue_dict[queue_type]
-        }
-        lobby_data=self.session.post(url,headers=self.headers,data=json.dumps(payload))
-        if primaryRole:
-            self.role_position(primaryRole,secondaryRole)
-        return lobby_data
 
 
-
-    def role_position(self,primary="FILL",secondary="FILL"):
-        """
-        Change the role for the lobby
-        Position names:
-            - TOP, BOTTOM, SUPPORT, JUNGLE, MIDDLE
-        primary: FILL
-            - your primary role for the game queue
-        secondary: FILL
-            - your secondary role for the game queue 
-        """
-        url=self.url+"/lol-lobby/v2/lobby/members/localMember/position-preferences"
-        primary,secondary = primary.upper(),secondary.upper()
-        payload={
-            "firstPreference": primary,
-            "secondPreference": secondary
-        }
-        position_data=self.session.put(url,data=json.dumps(payload))
-        return position_data
-
-    def send_invite(self,summoner_name:str):
-        """
-        Invite a summoner to your lobby
-        """
-        summoner_url=self.url+f"/lol-summoner/v1/summoners?name={summoner_name}"
-        summoner_data=self.session.get(summoner_url)
-        
-        if summoner_data.status_code == 404:
-            return f"Summoner {summoner_name} not found"
-        
-        summonerId=summoner_data.json()['summonerId']
-        invite_url=self.url+'/lol-lobby/v2/lobby/invitations'
-        payload=[{
-            "toSummonerId":summonerId
-        }]
-
-        invite_data=self.session.post(invite_url,data=json.dumps(payload))
-
-        return invite_data
-
-    def change_icon(self,iconId):
-        """
-        Change your summoner icon.
-        """
-        url = self.url+f"/lol-summoner/v1/current-summoner/icon"
-        data = self.session.put(url,data=json.dumps({"profileIconId":iconId}))
-
-        return data.ok
-
-
-    def queue_check(self):
-        """
-        Check if a match has been found.
-        """
-        url = self.url+ "/lol-matchmaking/v1/ready-check"
-        data= self.session.get(url)
-        return data
-
+class ChampSelect(LcuSessionSetup):
+    def __init__(self) -> None:
+        super().__init__()
 
 
     def play_again(self):
         #Play the same game mode again, returns back into lobby
         url = self.url+'/lol-lobby/v2/play-again'
         data = self.session.post(url)
-        return data
-
-    def search_state(self) -> Response:
-        """
-        Check the state of the match search
-        Invalid: Not in queue
-        Searching: In queue for match.
-        """
-        url = self.url+ "/lol-lobby/v2/lobby/matchmaking/search-state"
-        data = self.session.get(url)
-        return data
-
-
-    def penalty_countdown(self):
-        # count down timer for match found??
-        #This function should return bool to indicate if there's a penalty.
-        url = self.url + '/lol-lobby/v2/lobby/matchmaking/search-state'
-        data = self.session.get(url).json()
-        return data['lowPriorityData']['penaltyTimeRemaining']
-
-
-    def start_match(self):
-        self.find_match()
-
-
-
-    def find_match(self):
-        """
-        Start the queue for the lobby.
-
-
-        ERRORS:
-            BAD_METADATA: No positions selected
-            INVALID_POSITION_PREFERENCES: Only one role selected and maybe party member do not cover enough roles.
-        """
-        url = self.url+"/lol-lobby/v2/lobby/matchmaking/search"
-        data = self.session.post(url)
-        return data
-
-
-    def decline_match(self):
-        url = self.url + "/lol-lobby-team-builder/v1/ready-check/decline"
-        data = self.session.post(url)
-        return data
-
-
-
-    def accept_match(self):
-        #accept match when found
-        url = self.url+"/lol-matchmaking/v1/ready-check/accept"
-        data  = self.session.post(url)
         return data
 
 
@@ -352,6 +193,42 @@ class LCU(object):
         return data['actions'][-1][0]['type']
 
 
+
+
+
+    def bannable_campions(self) -> Response:
+        """
+        """
+        url = self.url + "/lol-champ-select/v1/bannable-champions"
+        data = self.session.get(url)
+        return data
+
+
+    def check_selected_champion(self) -> Response:
+        """
+        Checks what champion the local summoner currently has LOCKED IN
+        """
+        url = self.url+"/lol-champ-select/v1/current-champion"
+        data = self.session.get(url)
+        return data
+
+
+class Summoner(LcuSessionSetup):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+
+    def change_icon(self,iconId):
+        """
+        Change your summoner icon.
+        """
+        url = self.url+f"/lol-summoner/v1/current-summoner/icon"
+        data = self.session.put(url,data=json.dumps({"profileIconId":iconId}))
+
+        return data.ok
+
+
     def export_friends(self,path=None):
         """
         - path
@@ -372,6 +249,188 @@ class LCU(object):
         return data
 
 
+    def logout(self) -> Response:
+        """
+        Logout the current summoner account
+        """
+        url = self.url+"/lol-login/v1/session"
+        data = self.session.delete(url)
+        return data
+
+
+
+
+
+class Lobby(LcuSessionSetup):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    def leave_lobby(self):
+        """
+        Leaves the lobby
+        NOTE: DO NOT RUN THIS FUNCTION WHILE STILL DURING THE HONOR TEAMMATE SCREEN TO AVOID BLACK SCREEN
+        """
+        url=self.url+"/lol-lobby/v2/lobby"
+        data = self.session.delete(url)
+        return data
+    
+    def create_lobby(self,queue_type:str="DRAFT",primaryRole = None, secondaryRole = "FILL"):
+        """
+        Create a lobby with given lobby type
+        Check the `queue_dict` dict for lobby options
+        THIS FUNCTION CAN ALSO SET THE ROLE POSITIONS IF PROVIDED
+            - NOTE: NOT SETTING THE SECONDARY ROLE WILL MAKE YOU FILL AS SECONDARY 
+        """
+        queue_type= queue_type.upper()
+        queue_dict= {
+            "DRAFT": 400,
+            "RANKED":420,
+            "RANKED_FLEX":440,
+            "ARAM":450,
+            "BLIND":430,
+            "CUSTOM":"CUSTOM_GAME",
+            "PRACTICE":"PRACTICETOOL"
+        }
+        url=self.url+"/lol-lobby/v2/lobby"
+
+        if queue_type in ['CUSTOM_GAME','PRACTICE']:
+            data = {
+                "customGameLobby": {
+                    "configuration": {
+                    "gameMode": f"{queue_dict[queue_type]}", "gameMutator": "", "gameServerRegion": "", "mapId": 11, "mutators": {"id": 1}, "spectatorPolicy": "AllAllowed", "teamSize": 5
+                    },
+                    "lobbyName": "Name",
+                    "lobbyPassword": None
+                },
+                "isCustom": True
+            }
+            self.session.post(url,data=json.dumps(data))
+            
+
+        if queue_type not in queue_dict:
+            return f"Queue type {queue_type} not found."
+
+        payload={
+            "queueId": queue_dict[queue_type]
+        }
+        lobby_data=self.session.post(url,data=json.dumps(payload))
+        if primaryRole:
+            self.role_position(primaryRole,secondaryRole)
+        return lobby_data
+
+
+
+    def role_position(self,primary="FILL",secondary="FILL"):
+        """
+        Change the role for the lobby
+        Position names:
+            - TOP, BOTTOM, SUPPORT, JUNGLE, MIDDLE
+        primary: FILL
+            - your primary role for the game queue
+        secondary: FILL
+            - your secondary role for the game queue 
+        """
+        url=self.url+"/lol-lobby/v2/lobby/members/localMember/position-preferences"
+        primary,secondary = primary.upper(),secondary.upper()
+        payload={
+            "firstPreference": primary,
+            "secondPreference": secondary
+        }
+        position_data=self.session.put(url,data=json.dumps(payload))
+        return position_data
+
+    def send_invite(self,summoner_name:str):
+        """
+        Invite a summoner to your lobby
+        """
+        summoner_url=self.url+f"/lol-summoner/v1/summoners?name={summoner_name}"
+        summoner_data=self.session.get(summoner_url)
+        
+        if summoner_data.status_code == 404:
+            return f"Summoner {summoner_name} not found"
+        
+        summonerId=summoner_data.json()['summonerId']
+        invite_url=self.url+'/lol-lobby/v2/lobby/invitations'
+        payload=[{
+            "toSummonerId":summonerId
+        }]
+
+        invite_data=self.session.post(invite_url,data=json.dumps(payload))
+
+        return invite_data
+
+
+    def queue_check(self):
+        """
+        Check if a match has been found.
+        """
+        url = self.url+ "/lol-matchmaking/v1/ready-check"
+        data= self.session.get(url)
+        return data
+
+
+    def search_state(self) -> Response:
+        """
+        Check the state of the match search
+        Invalid: Not in queue
+        Searching: In queue for match.
+        """
+        url = self.url+ "/lol-lobby/v2/lobby/matchmaking/search-state"
+        data = self.session.get(url)
+        return data
+
+
+    def penalty_countdown(self):
+        # count down timer for match found??
+        #This function should return bool to indicate if there's a penalty.
+        url = self.url + '/lol-lobby/v2/lobby/matchmaking/search-state'
+        data = self.session.get(url).json()
+        return data['lowPriorityData']['penaltyTimeRemaining']
+
+
+    def start_match(self):
+        #alias for match
+        self.find_match()
+
+
+    def leave_queue(self):
+        """
+        Leave the current queue 
+        """
+        url = self.url+"/lol-lobby/v2/lobby/matchmaking/search"
+        data = self.session.delete(url)
+        return data
+
+
+
+    def find_match(self):
+        """
+        Start the queue for the lobby.
+
+
+        ERRORS:
+            BAD_METADATA: No positions selected
+            INVALID_POSITION_PREFERENCES: Only one role selected and maybe party member do not cover enough roles.
+        """
+        url = self.url+"/lol-lobby/v2/lobby/matchmaking/search"
+        data = self.session.post(url)
+        return data
+
+
+    def decline_match(self):
+        url = self.url + "/lol-lobby-team-builder/v1/ready-check/decline"
+        data = self.session.post(url)
+        return data
+
+
+
+    def accept_match(self):
+        #accept match when found
+        url = self.url+"/lol-matchmaking/v1/ready-check/accept"
+        data  = self.session.post(url)
+        return data
+
     def queue_info(self):
         queueState = self.queue_check()
         if queueState.status_code == 200:
@@ -380,21 +439,11 @@ class LCU(object):
         
         return False
 
-    def bannable_campions(self) -> Response:
-        """
-        """
-        url = self.url + "/lol-champ-select/v1/bannable-champions"
-        data = self.session.get(url)
-        return data
 
-
-    def logout(self) -> Response:
-        """
-        Logout the current summoner account
-        """
-        url = self.url+"/lol-login/v1/session"
-        data = self.session.delete(url)
-        return data
+class LCU(Lobby):
+    def __init__(self):
+        super().__init__()
+        # self.session.verify=os.path.join(os.path.dirname(os.path.abspath(__file__)),'riotgames.pem')
 
 
     @property
@@ -407,22 +456,6 @@ class LCU(object):
         return True
 
  
-
-
-    def check_selected_champion(self) -> Response:
-        """
-        Checks what champion the local summoner currently has LOCKED IN
-        """
-        url = self.url+"/lol-champ-select/v1/current-champion"
-        data = self.session.get(url)
-        return data
-
-
-
-    def gg(self,url):
-        data = self.session.get(self.url+url)
-        print(data.text)
-        return data
 
 
 
@@ -452,7 +485,7 @@ class AutoFunctions(LCU):
 
 
 
-    def auto_accept_match(self,wait_time:int=3) -> tuple[Response,bool]:
+    def auto_accept_match(self,wait_time:int=3) -> Response:
         """
         The function will not work if the lobby isn't in queue already and will stop when queue has been cancelled
         - wait_time
@@ -489,6 +522,6 @@ class AutoFunctions(LCU):
         self.ban_pick_champion(championName)
 
 
-autos = AutoFunctions()
+# autos = AutoFunctions()
 lol = LCU()
-lol.create_lobby("ranked",'bottom','middle')
+lol.create_lobby("DRAFT","bottom",'middle')
